d1e50ef67e08423d0bab627f9edb2972425bf5e3
Jayaraman2208 <jayraman441@gmail.com>
2026-09-26 15:30:03 +0530

    Restore standard Django config for Vercel zero-config deployment

--- api/index.py
+++
@@ -1,3 +0,0 @@
-﻿from delivery_management_system.wsgi import application
-
-app = application
--- delivery_management_system/settings.py
+++ delivery_management_system/settings.py
@@ -55,7 +55,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'delivery_management_system.wsgi.application'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
